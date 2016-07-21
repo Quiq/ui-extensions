2dc491cdbf5aea6674562c231bf025b03a4bfafd
Nate Norberg <natenorberg@gmail.com>
2016-07-20 18:03:16 -0600

    Migrated SDK from GitLab to GitHub

---
+++ README.md
@@ -0,0 +1,201 @@
+Centricient Extension SDK
+=========================
+
+This is an SDK to simplify communications between the Centricient
+messaging client and any extensions that are added to it.
+
+An extension is just a webpage that can be loaded into an iframe in the
+Centricient messaging client to add additional functionality. _This webpage must
+also be hosted from a site using https._
+
+## Usage
+
+The SDK exports the `Centricient` object to the window of the browser.
+Just include the SDK in a script tag before your code. At some point
+this should also be included as a node module so you could just use:
+
+```javascript
+var Centricient = require('two-cents');
+```
+
+To send messages back to the Centricient client, you also need to call `init`
+and pass the hostname of the page you're hosting the extension in.
+
+```javascript
+Centricient.init('https://mycompany.centricient.corp');
+```
+
+## Events
+
+You can subscribe to events using the `on` method.
+
+#### Syntax
+
+```javascript
+Centricient.on(eventName, handler);
+```
+
+The eventName is a string of the type of event to listen to. The handler
+should be a function that accepts the event data as an argument.
+
+#### Example
+
+```javascript
+Centricient.on('messageAdded', function(data) {
+  console.log('Agent said ' + data.text);
+});
+```
+
+### Available events
+
+#### init
+
+Called when the add-in first loads
+
+Data:
+- `conversationId` (string) - The id of the conversation
+- `conversation` (object) - The conversation object
+
+
+#### conversationAccepted
+
+Called when the agent accepts a conversation
+
+Data:
+- `conversationId` (string) - The id of the conversation
+
+#### messageAdded
+
+Called when the agent sends a message to the customer
+
+Data:
+- `conversationId` (string) - The id of the conversation
+- `text` (string) - The text of the message sent
+
+
+#### messageReceived
+
+Called when the customer sends a message to the agent
+
+Data:
+- `conversationId` (string) - The id of the conversation
+- `message` (object) - The message object received by the agent
+
+
+#### conversationStatusChanged
+
+Called when the conversation is marked active or inactive
+
+Data:
+- `id` (string) - The id of the conversation
+- `status` (string) - The new status: `'active'` or `'inactive'`
+
+
+#### extensionDataChanged
+
+Called when the extension data string changes
+
+Data:
+- `data` (string) - The new extension data value
+- `conversationId` (string)- The id of the conversation
+- `extensionId` (string)- The id of the extension
+
+
+## Actions
+
+You can dispatch actions in the Centricient client by calling methods on
+`Centricient`.
+
+
+### `prepareMessage(text)`
+
+Prepares a message to send the customer, but doesn't send it
+
+Arguments:
+- `message` (string) - The message to send to the customer
+
+
+## Data
+
+You can get data for the context of the extension by using the getters
+provided by `Centricient`. You can only read this data. The only way to
+modify data is by using actions.
+
+### Accessing conversation data
+
+```javascript
+var conversation = Centricient.getConversation();
+```
+
+#### Conversation object
+- `id` (string) - The id of the conversation
+- `owner` (string) - The id of the agent the conversation is assigned to
+- `contact` (Contact?) - The contact data for the conversation (or `null` if conversation is a collaboration)
+- `messages` (array<Message>) - The array of messages for the conversation
+- `collaboration` (Conversation?) - The conversation object of the collaboration (or `null` if there isn't one)
+- `metrics` (ConversationMetrics) - Some metrics for the conversation
+
+#### Contact object
+- `firstName` (string)
+- `lastName` (string)
+- `phoneNumber` (string) - If contact is using SMS
+- `facebookId` (string) - If contact is using Facebook
+- `externalId` (string) - If there is an integration with another CRM
+
+#### Message object
+- `id` (string) - The id of the message
+- `text` (string) - The text of the message
+- `timestamp` (number) - The timestamp of the message
+- `author` (string) - The agentId (if sent by an agent) or whatever id is used for the channel the customer is using (i.e. phoneNumber or facebookId)
+- `fromCustomer` (boolean) - If the message is from the customer or not
+- `sourcePlatform` (string) - What platform the message was sent over (i.e. 'SMS', 'Facebook', 'Centricient')
+
+#### ConversationMetrics
+- `startTime` (number) - The timestamp for when the conversation was started
+- `endTime` (number?) - The timestamp for when the conversation ended, or `null` if it hasn't yet
+
+### Getting id's
+
+You can also get the userId for the logged in agent, as well as the id for the
+tenant (if your extension is for more than one company)
+
+```javascript
+var userId = Centricient.getUserId();
+var tenantId = Centricient.getTenantId();
+```
+
+### Extension data
+
+You can store a string of data on the conversation object which will be persisted
+along with the conversation data. It must be stored as a string, but you can use
+`JSON.stringify` and `JSON.parse` to store more complex data.
+
+You can use `getExtensionData` to retrieve the current value of the data and
+`setExtensionData` to update the value on the server. `setExtensionData` returns
+a promise so that you can fire additional events after the request is successful,
+or hook up any error handling if the save fails for some reason.
+
+```javascript
+// Get the current value of your extension data
+var data = Centricient.getExtensionData();
+
+var newData = { foo: 'bar' };
+
+// Save the new data
+Centricient.setExtensionData(JSON.stringify(newData))
+  .then(function() {
+    console.log('Data is saved');
+  });
+```
+
+## Async data
+
+You can fetch data for the addin by calling `fetchUsers` which is a
+function that returns a promise. Eventually we'll be able to add more of
+these.
+
+```javascript
+Centricient.fetchUsers().then(function(data) {
+  // Do something with the users data
+});
+```
---
+++ sdk.js
@@ -0,0 +1,172 @@
+// Promise polyfill from https://github.com/taylorhakes/promise-polyfill
+!function(t){function e(){}function n(t,e){return function(){t.apply(e,arguments)}}function o(t){if("object"!=typeof this)throw new TypeError("Promises must be constructed via new");if("function"!=typeof t)throw new TypeError("not a function");this._state=0,this._handled=!1,this._value=void 0,this._deferreds=[],s(t,this)}function r(t,e){for(;3===t._state;)t=t._value;return 0===t._state?void t._deferreds.push(e):(t._handled=!0,void a(function(){var n=1===t._state?e.onFulfilled:e.onRejected;if(null===n)return void(1===t._state?i:f)(e.promise,t._value);var o;try{o=n(t._value)}catch(r){return void f(e.promise,r)}i(e.promise,o)}))}function i(t,e){try{if(e===t)throw new TypeError("A promise cannot be resolved with itself.");if(e&&("object"==typeof e||"function"==typeof e)){var r=e.then;if(e instanceof o)return t._state=3,t._value=e,void u(t);if("function"==typeof r)return void s(n(r,e),t)}t._state=1,t._value=e,u(t)}catch(i){f(t,i)}}function f(t,e){t._state=2,t._value=e,u(t)}function u(t){2===t._state&&0===t._deferreds.length&&a(function(){t._handled||d(t._value)});for(var e=0,n=t._deferreds.length;n>e;e++)r(t,t._deferreds[e]);t._deferreds=null}function c(t,e,n){this.onFulfilled="function"==typeof t?t:null,this.onRejected="function"==typeof e?e:null,this.promise=n}function s(t,e){var n=!1;try{t(function(t){n||(n=!0,i(e,t))},function(t){n||(n=!0,f(e,t))})}catch(o){if(n)return;n=!0,f(e,o)}}var l=setTimeout,a="function"==typeof setImmediate&&setImmediate||function(t){l(t,0)},d=function(t){"undefined"!=typeof console&&console&&console.warn("Possible Unhandled Promise Rejection:",t)};o.prototype["catch"]=function(t){return this.then(null,t)},o.prototype.then=function(t,n){var o=new this.constructor(e);return r(this,new c(t,n,o)),o},o.all=function(t){var e=Array.prototype.slice.call(t);return new o(function(t,n){function o(i,f){try{if(f&&("object"==typeof f||"function"==typeof f)){var u=f.then;if("function"==typeof u)return void u.call(f,function(t){o(i,t)},n)}e[i]=f,0===--r&&t(e)}catch(c){n(c)}}if(0===e.length)return t([]);for(var r=e.length,i=0;i<e.length;i++)o(i,e[i])})},o.resolve=function(t){return t&&"object"==typeof t&&t.constructor===o?t:new o(function(e){e(t)})},o.reject=function(t){return new o(function(e,n){n(t)})},o.race=function(t){return new o(function(e,n){for(var o=0,r=t.length;r>o;o++)t[o].then(e,n)})},o._setImmediateFn=function(t){a=t},o._setUnhandledRejectionFn=function(t){d=t},"undefined"!=typeof module&&module.exports?module.exports=o:t.Promise||(t.Promise=o)}(this);
+
+/**
+* The SDK for creating Centricient Messaging add-ins
+*/
+(function() {
+'use strict';
+
+var Centricient = {};
+// TODO: Figure out how to get this dynamically
+var centricientHost;
+var eventHandlers = {};
+var extensionId;
+var conversationId;
+var conversation = {};
+var userId, tenantId, extensionData;
+
+function postMessageToApp(eventType, data) {
+  if (!centricientHost) {
+    throw new Error('You need to call `init` before posting messages to Centricient');
+  }
+  window.parent.postMessage({eventType: eventType, data: data }, centricientHost);
+}
+
+function checkConversationId() {
+  if (!conversationId) {
+    console.error('Can\'t send a message without a conversationId. '
+      + 'Verify addIn initialized properly');
+  }
+}
+
+Centricient.init = function(host) {
+  if (!host) {
+    throw new Error('Init needs to be called with the hostname of the site that will run ')
+  }
+  centricientHost = host;
+};
+
+Centricient.on = function(eventName, handler) {
+  eventHandlers[eventName] = eventHandlers[eventName] || [];
+  eventHandlers[eventName].push(handler);
+};
+
+// TODO: We might want to create a way to register an object of handlers all at once
+
+/**
+ * Sends a message in the conversation that the add-in is attached to
+ * @param  {string} message - The message to send
+ */
+Centricient.sendMessage = function(message) {
+  checkConversationId();
+  postMessageToApp('sendMessage', { conversationId: conversationId, message: message });
+}
+
+/**
+ * Adds a message to the conversation that the add-in is attached to, but doesn't send it
+ * @param  {string} message - The message to send
+ */
+Centricient.prepareMessage = function(message) {
+  checkConversationId();
+  postMessageToApp('prepareMessage', { conversationId: conversationId, message: message });
+}
+
+/**
+ * Gets the current conversation object
+ * @return {object} The conversation object
+ */
+Centricient.getConversation = function() {
+  // TODO: Should we just add `conversation` to Centricient instead of making a getter?
+  return conversation;
+};
+
+/**
+ * Gets the userId for the agent currently logged in
+ */
+Centricient.getUserId = function() {
+  return userId;
+};
+
+/**
+ * Gets the id for the centricient tenant running the extension
+ */
+Centricient.getTenantId = function() {
+  return tenantId;
+};
+
+var requestCounter = 0;
+
+function asyncRequest(requestType, data) {
+  return new Promise(function(resolve, reject) {
+    Centricient.on(requestType + '.success' + requestCounter, function(data) {
+      resolve(data.data);
+    });
+
+    Centricient.on(requestType + '.error' + requestCounter, function(error) {
+      reject(error);
+    });
+
+    postMessageToApp(requestType, {
+      requestCounter: requestCounter,
+      conversationId: conversationId,
+      extensionId: extensionId,
+      data: data,
+    });
+    requestCounter++;
+  })
+}
+
+// Just a test of async data fetching for addins. We probably won't use this one
+Centricient.fetchUsers = function() {
+  return asyncRequest('fetchUsers');
+};
+
+/**
+ * Gets the conversation extension data for the current extension
+ * @returns {Promise}
+ */
+Centricient.getExtensionData = function() {
+  return extensionData;
+}
+
+/**
+ * Sets the field of external data that we have on a conversation for extensions
+ * @param {string} data - The blob of data to add to the conversation
+ * @returns {Promise} A promise so you can know if the request succeeded or failed
+ */
+Centricient.setExtensionData = function(data) {
+  var oldExtensionData = extensionData;
+  extensionData = data;
+
+  return new Promise(function(resolve, reject) {
+    asyncRequest('setExtensionData', data).then(resolve)
+      .catch(function(error) {
+        extensionData = oldExtensionData;
+        reject(error);
+      });
+  });
+}
+
+
+function handleEvent(event) {
+  var eventType = event.data.eventType;
+  var data = event.data.data;
+
+  if (eventType === 'init') {
+    conversationId = data.conversationId;
+    conversation = data.conversation;
+    userId = data.userId;
+    tenantId = data.tenantId;
+    extensionId = data.extensionId;
+    extensionData = data.extensionData;
+  }
+  else if (eventType === 'extensionDataChanged') {
+    extensionData = data.data;
+  }
+
+  console.log('Handling event', event);
+
+  var handlers = eventHandlers[eventType];
+  if (handlers) {
+    handlers.forEach(function(handler) {
+      handler(data);
+    });
+  }
+}
+
+window.addEventListener('message', handleEvent, false);
+
+// Export to global namespace. This feels yucky
+window.Centricient = Centricient;
+})()
