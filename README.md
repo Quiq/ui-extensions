56fc6dbb6086d468258536df86e791709a1de0ba
Fred Vollmer <fredvollmer@Freds-MacBook-Pro.local>
2018-05-16 13:17:39 -0600

    Revert "New documentation site"
    This reverts commit 73ed54008929c95367875baf158bc1a283578ed7.

--- README.md
+++ README.md
@@ -1,4 +1,256 @@
 Quiq Extension SDK
 =========================
 
-### This project is no longer hosted on GitHub, but is still actively maintained and improved. Complete documentation can be found at <https://developers.goquiq.com/docs/ui-extensions/>
+[![Build Status](https://travis-ci.org/Quiq/ui-extensions.svg?branch=master)](https://travis-ci.org/Quiq/ui-extensions)
+
+This is an SDK to simplify communications between the Quiq
+messaging client and any extensions that are added to it.
+
+An extension is just a webpage that can be loaded into an iframe in the
+Quiq messaging client to add additional functionality. _This webpage must
+also be hosted from a site using https._
+
+## Example
+We have provided a basic example on how to initialize and receive data from Quiq.  
+To see and use this example, run either `npm run start` or `yarn start`,
+navigate to `http://localhost:3000/example`, and follow the instructions.  
+For this to work, you must have [python](https://www.python.org/downloads/) and either [npm](https://nodejs.org/en/) or [yarn](https://yarnpkg.com/en/) installed.
+You will also need [ngrok](https://ngrok.com/).
+
+## Usage
+
+The SDK exports the `Quiq` object to the window of the browser.
+Just include the SDK in a script tag before your code.
+
+To send messages back to the Quiq client, you also need to call `init`
+and pass the hostname of the page you're hosting the extension in.
+
+```javascript
+Quiq.init('https://mycompany.goquiq.com');
+```
+
+### Cache busting
+Due to various problems across browsers with iframes, you will likely want to add this
+code to your HTML page.
+Without this code, you and your users will likely experience a significant delay
+before any page modifications are available.
+
+```html
+<head>
+    <meta http-Equiv="Cache-Control" Content="no-cache" />
+    <meta http-Equiv="Pragma" Content="no-cache" />
+    <meta http-Equiv="Expires" Content="0" />
+</head>
+```
+
+## Events
+
+You can subscribe to events using the `on` method.
+
+#### Syntax
+
+```javascript
+Quiq.on(eventName, handler);
+```
+
+The eventName is a string of the type of event to listen to. The handler
+should be a function that accepts the event data as an argument.
+
+#### Example
+
+```javascript
+Quiq.on('messageAdded', function(data) {
+  console.log('Agent said ' + data.text);
+});
+```
+
+### Available events
+
+#### init
+
+Called when the add-in first loads
+
+Data:
+- `conversation` (object) - The conversation object
+
+
+#### conversationAccepted
+
+Called when the agent accepts a conversation
+
+Data:
+- `conversationId` (string) - The id of the conversation
+
+#### messageAdded
+
+Called when the agent sends a message to the customer
+
+Data:
+- `conversationId` (string) - The id of the conversation
+- `text` (string) - The text of the message sent
+
+
+#### messageReceived
+
+Called when the customer sends a message to the agent
+
+Data:
+- `conversationId` (string) - The id of the conversation
+- `message` (object) - The message object received by the agent
+
+#### collaborationMessageReceived
+
+Called when a collaborator sends a message in the collaboration
+
+Data:
+- `conversationId` (string) - The id of the service conversation
+- `collaborationId` (string) - The id of the collaboration
+- `message` (object) - The message object received by the agent
+
+#### conversationStatusChanged
+
+Called when the conversation is marked active or inactive
+
+Data:
+- `id` (string) - The id of the conversation
+- `status` (string) - The new status: `'active'` or `'inactive'`
+
+
+#### extensionDataChanged
+
+Called when the extension data string changes
+
+Data:
+- `data` (string) - The new extension data value
+- `conversationId` (string)- The id of the conversation
+- `extensionId` (string)- The id of the extension
+
+
+## Actions
+
+You can dispatch actions in the Quiq client by calling methods on
+`Quiq`.
+
+
+### `prepareMessage(text, method? = 'replace')`
+
+Prepares a message to send the customer, but doesn't send it
+
+Arguments:
+- `message` (string) - The message to send to the customer
+- `method` ('replace' | 'append' | 'prepend') - Whether the text should replace the text in the textbox or be appended or prepended to it
+
+### `resizeWindow(dimensions)`
+
+Resizes the iFrame container to the given dimensions
+
+Arguments:
+- `dimensions` (Object: { height?: string|number, width?: string|number }) - New dimensions to size the window with. At least one of height or width must be defined.
+
+### `sendOnClose(text)`
+
+Sets a message to send as soon as the conversation is closed by the user.
+
+Arguments:
+- `message` (string) - The message to send to the customer
+
+A couple notes
+- The extension can only send one message at the end of the conversation. Calling this method more than once will change the message rather than queueing up new messages. If you call it with an empty string, it shoudn't send anything.
+- The message will only be sent if the conversation is ended by the user clicking the End button while the extension is loaded. It won't send it if the conversation is closed from inactivity or if it's closed by an agent that doesn't have the extension.
+- The message won't be sent if the conversation is marked as spam or if the user clicks End without ever responding
+- The message also won't send in the "long goodbye" case (i.e. The conversation is closed and then the customer texts back something that the agent clicks end without responding to)
+
+
+### `updateContactDisplayName(contact)`
+
+Updates the name shown in the app for the contact of the conversation. This is only for SMS conversations and the name will be saved on the server in the conversation object.
+
+Arguments:
+- `contact` (Object: {firstName:string, lastName:string}) - An object containing the firstName and lastName of the contact
+
+
+## Data
+
+You can get data for the context of the extension by using the getters
+provided by `Quiq`. You can only read this data. The only way to
+modify data is by using actions.
+
+### Accessing conversation data
+
+```javascript
+var conversation = Quiq.getConversation();
+```
+
+#### Conversation object
+- `id` (string) - The id of the conversation
+- `owner` (string?) - The id of the agent the conversation is assigned to if it's assigned
+- `contact` (Contact?) - The contact data for the conversation (or `null` if conversation is a collaboration)
+- `messages` (array<Message>) - The array of messages for the conversation
+- `collaboration` (Conversation?) - The conversation object of the collaboration (or `null` if there isn't one)
+- `metrics` (ConversationMetrics) - Some metrics for the conversation
+- `contactPointId` (string?) - The contact point id of the conversation
+
+#### Contact object
+- `firstName` (string)
+- `lastName` (string)
+- `phoneNumber` (string) - If contact is using SMS
+- `facebookId` (string) - If contact is using Facebook
+- `externalId` (string) - If there is an integration with another CRM
+
+#### Message object
+- `id` (string) - The id of the message
+- `text` (string?) - The text of the message
+- `timestamp` (number) - The timestamp of the message
+- `author` (string) - The agentId (if sent by an agent) or whatever id is used for the channel the customer is using (i.e. phoneNumber or facebookId)
+- `fromCustomer` (boolean) - If the message is from the customer or not
+- `sourcePlatform` (string) - What platform the message was sent over (i.e. 'SMS', 'Facebook', 'Quiq')
+
+#### ConversationMetrics
+- `startTime` (number) - The timestamp for when the conversation was started
+- `endTime` (number?) - The timestamp for when the conversation ended, or `null` if it hasn't yet
+
+### Getting id's
+
+You can also get the userId for the logged in agent, as well as the id for the
+tenant (if your extension is for more than one company)
+
+```javascript
+var userId = Quiq.getUserId();
+var tenantId = Quiq.getTenantId();
+```
+
+### Extension data
+
+You can store a string of data on the conversation object which will be persisted
+along with the conversation data. It must be stored as a string, but you can use
+`JSON.stringify` and `JSON.parse` to store more complex data.
+
+You can use `getExtensionData` to retrieve the current value of the data and
+`setExtensionData` to update the value on the server. `setExtensionData` returns
+a promise so that you can fire additional events after the request is successful,
+or hook up any error handling if the save fails for some reason.
+
+```javascript
+// Get the current value of your extension data
+var data = Quiq.getExtensionData();
+
+var newData = { foo: 'bar' };
+
+// Save the new data
+Quiq.setExtensionData(JSON.stringify(newData))
+  .then(function() {
+    console.log('Data is saved');
+  });
+```
+
+## Async data
+
+You can fetch data for the addin by calling `fetchUsers` which is a
+function that returns a promise. Eventually we'll be able to add more of
+these.
+
+```javascript
+Quiq.fetchUsers().then(function(data) {
+  // Do something with the users data
+});
+```
---
+++ __tests__/sdk.test.js
@@ -0,0 +1,387 @@
+require('../sdk'); // Add Quiq to window
+
+const conversationId = 'conversationId';
+const userId = 'userId';
+const tenantId = 'tenantId';
+const extensionId = 'extensionId';
+const testHost = 'https://sdk-test.centricient.com';
+
+const testContact = {
+  externalId: '+14065551234',
+  phoneNumber: '+14065551234',
+  facebookId: null,
+  firstName: 'Lauren',
+  lastName: 'Ipsum',
+};
+
+const testConversation = {
+  id: conversationId,
+  owner: 'userId',
+  status: 'active',
+  contact: testContact,
+  messages: [{
+    id: 'message1',
+    text: 'Marco',
+    author: '+14065551234',
+    timestamp: 1234567,
+    sourcePlatform: 'SMS',
+    fromCustomer: true,
+  }, {
+    id: 'message2',
+    text: 'Polo',
+    author: 'userId',
+    timestamp: 1234569,
+    sourcePlatform: 'SMS',
+    fromCustomer: false,
+  }],
+  metrics: {startTime: 1234567, endTime: null},
+};
+
+const simulateEvent = (eventType, data) => {
+  const testEvent = new MessageEvent('message', {
+    data: { eventType, data },
+  });
+  window.dispatchEvent(testEvent);
+};
+
+describe('Quiq SDK', () => {
+  beforeEach(() => {
+    window.parent.postMessage = jest.fn();
+    simulateEvent('init', {
+      conversationId,
+      conversation: testConversation,
+      userId,
+      tenantId,
+      extensionId,
+      extensionData: JSON.stringify({favCookie:'chocolate chip'}),
+    });
+  });
+
+  describe('before calling init', () => {
+    it('does not allow sending messages to the messaging app', () => {
+      const initError = 'You need to call `init` before posting messages to Quiq';
+      expect(Quiq.prepareMessage).toThrowError(initError);
+      expect(Quiq.sendOnClose).toThrowError(initError);
+      expect(Quiq.updateContactDisplayName).toThrowError(initError);
+    });
+  });
+
+
+  describe('data', () => {
+    describe('getConversation', () => {
+      it('returns the conversation object', () => {
+        expect(Quiq.getConversation()).toEqual(testConversation);
+      });
+    });
+
+    describe('getUserId', () => {
+      it('returns the userId', () => {
+        expect(Quiq.getUserId()).toEqual(userId);
+      });
+    });
+
+    describe('getTenantId', () => {
+      it('returns the tenantId', () => {
+        expect(Quiq.getTenantId()).toEqual(tenantId);
+      });
+    });
+
+    describe('getExtensionData', () => {
+      it('returns the extension data', () => {
+        expect(JSON.parse(Quiq.getExtensionData())).toEqual({favCookie: 'chocolate chip'});
+      });
+    });
+
+    describe('setExtensionData', () => {
+      const successHandler = jest.fn();
+      const errorHandler = jest.fn();
+      const newData = JSON.stringify({favCookie: 'peanut butter'});
+
+      beforeEach(() => {
+        Quiq.init(testHost);
+      });
+
+      // requestCounter is at 0 here
+      describe('when save is successful', () => {
+        it('posts the message to the app and calls the success callback', () => {
+          window.parent.postMessage.mockImplementationOnce(() => {
+            simulateEvent('setExtensionData.success0', {data: newData});
+          });
+
+          return Quiq.setExtensionData(newData).then((data) => {
+            expect(data).toEqual(newData);
+            expect(window.parent.postMessage).toBeCalledWith({
+              eventType: 'setExtensionData',
+              data: {
+                requestCounter: 0,
+                conversationId,
+                extensionId,
+                data: newData,
+              },
+            }, testHost);
+          });
+        });
+      });
+
+      // requestCounter is at 1 here
+      describe('when save is not successful', () => {
+        it('posts the message to the app and calls the error callback', () => {
+          window.parent.postMessage.mockImplementationOnce(() => {
+            simulateEvent('setExtensionData.error1', 'uh oh!');
+          });
+
+          return Quiq.setExtensionData(newData).then(() => {}, (error) => {
+            expect(error).toEqual('uh oh!');
+            expect(window.parent.postMessage).toBeCalledWith({
+              eventType: 'setExtensionData',
+              data: {
+                requestCounter: 1,
+                conversationId,
+                extensionId,
+                data: newData,
+              },
+            }, testHost);
+          });
+        });
+      });
+    });
+
+    describe('fetchUsers', () => {
+      const users = [
+        {id: 'thing1', firstName: 'Thing', lastName: '1'},
+        {id: 'thing2', firstName: 'Thing', lastName: '2'},
+      ];
+
+      it('posts the message to the app and calls the success callback', () => {
+        window.parent.postMessage.mockImplementationOnce(() => {
+          simulateEvent('fetchUsers.success2', {conversationId, data: users});
+        });
+
+        return Quiq.fetchUsers().then((data) => {
+          expect(data).toEqual(users);
+          expect(window.parent.postMessage).toBeCalledWith({
+            eventType: 'fetchUsers',
+            data: {
+              requestCounter: 2,
+              conversationId,
+              extensionId,
+              data: undefined,
+            },
+          }, testHost);
+        });
+      });
+    });
+  });
+
+  describe('events', () => {
+    describe('init', () => {
+      const initHandler = jest.fn();
+
+      beforeEach(() => {
+        Quiq.on('init', initHandler);
+        simulateEvent('init', {
+          conversationId,
+          conversation: testConversation,
+          userId,
+          tenantId,
+          extensionId,
+          extensionData: JSON.stringify({favCookie:'chocolate chip'}),
+        });
+      });
+
+      it('passes the conversation info', () => {
+        expect(initHandler).toBeCalledWith(jasmine.objectContaining(
+          {conversationId, conversation: testConversation}
+        ));
+      });
+    });
+
+    describe('conversationAccepted', () => {
+      const handler = jest.fn();
+
+      beforeEach(() => {
+        Quiq.on('conversationAccepted', handler);
+        simulateEvent('conversationAccepted', {conversationId});
+      });
+
+      it('passes the conversationId', () => {
+        expect(handler).toBeCalledWith({conversationId});
+      });
+    });
+
+    describe('messageAdded', () => {
+      const handler = jest.fn();
+
+      beforeEach(() => {
+        Quiq.on('messageAdded', handler);
+        simulateEvent('messageAdded', {conversationId, text: 'spongebob'});
+      });
+
+      it('passes the conversationId and message text', () => {
+        expect(handler).toBeCalledWith({conversationId, text: 'spongebob'});
+      });
+    });
+
+    describe('messageReceived', () => {
+      const handler = jest.fn();
+      const message = {
+        id: 'message3',
+        text: 'spongebob',
+        author: '+14065551234',
+        timestamp: 1234571,
+        sourcePlatform: 'SMS',
+        fromCustomer: true,
+      };
+
+      beforeEach(() => {
+        Quiq.on('messageReceived', handler);
+        simulateEvent('messageReceived', {conversationId, message});
+      });
+
+      it('passes the conversationId and message object', () => {
+        expect(handler).toBeCalledWith({conversationId, message});
+      });
+    });
+
+    describe('collaborationMessageReceived', () => {
+      const handler = jest.fn();
+      const collaborationId = 'collaborationId';
+      const message = {
+        id: 'message3',
+        text: 'spongebob',
+        author: 'collaborator',
+        timestamp: 1234571,
+        sourcePlatform: 'Quiq',
+        fromCustomer: false,
+      };
+
+      beforeEach(() => {
+        Quiq.on('collaborationMessageReceived', handler);
+        simulateEvent('collaborationMessageReceived', {conversationId, collaborationId, message});
+      });
+
+      it('passes the conversationId and message object', () => {
+        expect(handler).toBeCalledWith({conversationId, collaborationId, message});
+      });
+    });
+
+    describe('conversationStatusChanged', () => {
+      const handler = jest.fn();
+
+      beforeEach(() => {
+        Quiq.on('conversationStatusChanged', handler);
+        simulateEvent('conversationStatusChanged', {id: conversationId, status: 'inactive'});
+      });
+
+      it('passes the conversationId and new status', () => {
+        expect(handler).toBeCalledWith({id: conversationId, status: 'inactive'});
+      });
+    });
+
+    describe('extensionDataChanged', () => {
+      const handler = jest.fn();
+      const data = JSON.stringify({favCookie: 'peanut butter'});
+
+      beforeEach(() => {
+        Quiq.on('extensionDataChanged', handler);
+        simulateEvent('extensionDataChanged', {conversationId, extensionId, data});
+      });
+
+      it('passes the conversationId, extensionId, and new data', () => {
+        expect(handler).toBeCalledWith({conversationId, extensionId, data});
+      });
+
+      it('updates the value returned by getExtensionData', () => {
+        expect(JSON.parse(Quiq.getExtensionData())).toEqual({favCookie: 'peanut butter'});
+      });
+    });
+  });
+
+  describe('actions', () => {
+    describe('after calling init', () => {
+      beforeEach(() => {
+        Quiq.init(testHost);
+      });
+
+      describe('prepareMessage', () => {
+        it('posts an event to message-ui', () => {
+          Quiq.prepareMessage('hello', 'append');
+          expect(window.parent.postMessage).toBeCalledWith({
+            eventType: 'prepareMessage',
+            data: { conversationId, message: 'hello', method: 'append' },
+          }, testHost);
+        });
+      });
+
+      describe('sendOnClose', () => {
+        it('posts an event to message-ui', () => {
+          Quiq.sendOnClose('hello');
+          expect(window.parent.postMessage).toBeCalledWith({
+            eventType: 'sendOnClose',
+            data: { conversationId, message: 'hello' },
+          }, testHost);
+        });
+      });
+
+      describe('resizeWindow', () => {
+        it('validates input', () => {
+          const initError = 'Missing or malformed dimensions object for resizeWindow event';
+          expect(() => {
+            Quiq.resizeWindow('string')
+          }).toThrowError(initError);
+
+          expect(() => {
+            Quiq.resizeWindow(12345)
+          }).toThrowError(initError);
+
+          expect(() => {
+            Quiq.resizeWindow(null)
+          }).toThrowError(initError);
+
+          expect(() => {
+            Quiq.resizeWindow()
+          }).toThrowError(initError);
+
+          expect(() => {
+            Quiq.resizeWindow({})
+          }).toThrowError(initError);
+
+          expect(() => {
+            Quiq.resizeWindow({ width: '500px' })
+          }).not.toThrowError(initError);
+
+          expect(() => {
+            Quiq.resizeWindow({ height: '500px' })
+          }).not.toThrowError(initError);
+
+          expect(() => {
+            Quiq.resizeWindow({ height: '500px', width: '500px' })
+          }).not.toThrowError(initError);
+        });
+
+        it('posts valid params', () => {
+          const dimensions = { height: '500px', width: '500px' };
+          Quiq.resizeWindow(dimensions);
+          expect(window.parent.postMessage).toBeCalledWith({
+            eventType: 'resizeWindow',
+            data: { extensionId: 'extensionId', dimensions },
+          }, testHost);
+        });
+      });
+
+      describe('updateContactDisplayName', () => {
+        it('posts an event to message-ui', () => {
+          const newName = {firstName: 'Sherlock', lastName: 'Holmes'};
+          Quiq.updateContactDisplayName({
+            firstName: 'Sherlock',
+            lastName: 'Holmes',
+          });
+          expect(window.parent.postMessage).toBeCalledWith({
+            eventType: 'updateContactDisplayName',
+            data: { conversationId, contact: newName },
+          }, testHost);
+        });
+      });
+    });
+  });
+});
